45d2e51f9ce75afef816074952ba9fb633bd6aef
ManiPranshu <136685321+ManiPranshu@users.noreply.github.com>
2024-03-02 14:46:20 +0530

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,12 +6,18 @@ import '../App.css';
 function App() {
 
   const navbarStyle = {
-    backgroundColor: '#333',
-    overflow: 'hidden',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '10px 20px',
-  };
+    // backgroundColor: '#333',
+  //   overflow: 'hidden',
+  //   display: 'flex',
+  //   alignItems: 'center',
+  //   padding: '10px 20px',
+  // 
+  background: `rgba(51, 51, 51, 0.7) url(${process.env.PUBLIC_URL}/bg.png) center/cover no-repeat`,
+  overflow: 'hidden',
+  display: 'flex',
+  alignItems: 'center',
+  padding: '10px 20px',
+};
 
   const linkStyle = {
     color: '#f2f2f2',
@@ -24,14 +30,17 @@ function App() {
     height: '40px', // Adjust the height as needed
     marginRight: '10px',
   };
-  
+
   return (
     <div>
       <div style={navbarStyle}>
         <a href="#">
           <img src={process.env.PUBLIC_URL + '/logo3-1@2x.png'} alt="Logo" style={logoStyle} />
         </a>
-        {/* Rest of your navbar code... */}
+        <a href="#" style={linkStyle}>About</a>
+        <a href="#" style={linkStyle}>Blog</a>
+        <a href="#" style={linkStyle}>Projects</a>
+        <a href="#" style={linkStyle}>Team</a>
       </div>
       <div className="App" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/bg.png)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', width: '100vw', minHeight: '100vh', overflowY: 'scroll' }}>
         <p>
