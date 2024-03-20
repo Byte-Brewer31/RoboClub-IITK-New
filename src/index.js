3ed687e7a6b60ddf68029a402e7b32a4e12f650f
Devansh <devanshv17@gmail.com>
2024-03-21 00:31:37 +0530

    footer added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ import Navbar from './components/navbar';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router, Routes, Route
 
+const linkStyle = {
+  color: 'white',
+  textDecoration: 'none',
+  display: 'block',
+  fontSize: '2vw',
+  paddingBottom: '0.8vw',
+  fontWeight: 'bolder',
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -32,7 +41,46 @@ root.render(
         <Route path="/projects" element={<Projects/>} />
       </Routes>
     </Router>
-    </div>
+
+    <div style={{ 
+        display: 'flex',
+        alignItems: 'center',
+        padding: '5%', // Add padding for spacing
+        color: 'white', // Add color for better visibility
+      }}>
+        
+        <div style={{ width: '33%', textAlign: 'left', paddingLeft: '3vw' }}>
+        <img src={process.env.PUBLIC_URL + '/logo3-1@2x.png'} alt="Logo" style={{height: '5vw',margin: '1% 1%',}} />
+        </div>
+        <div style={{ width: '30%',display: 'flex', textAlign:'left', flexDirection: 'column', color: 'white', textDecoration: 'none', padding: '10px'}}>
+          <a href="/" style={linkStyle}>Home</a>
+          <a href="/about" style={linkStyle}>About</a>
+          <a href="/blog" style={linkStyle}>Blog</a>
+          <a href="/projects" style={linkStyle}>Projects</a>
+          <a href="/team" style={linkStyle}>Team</a>
+        </div>
+        <div style={{ width: '37%', textAlign:'left'}}>
+          <h3 style={{fontSize: '2vw', color: 'white', textAlign: 'left'}}>Social Media </h3>
+          <a href="#">
+          <img src={process.env.PUBLIC_URL + '/riyoutubefill.svg'} alt="Facebook Icon" style={{width: '6vw', height: 'auto', paddingRight:'1vw'}} />
+          </a>
+          <a href="#">
+          <img src={process.env.PUBLIC_URL + '/mdifacebook.svg'} alt="Facebook Icon" style={{width: '6vw', height: 'auto',paddingRight:'1vw'}} />
+          </a>
+          <a href="#">
+          <img src={process.env.PUBLIC_URL + '/phinstagramlogofill.svg'} alt="Facebook Icon" style={{width: '6vw', height: 'auto',paddingRight:'1vw'}} />
+          </a>
+          <a href="#">
+          <img src={process.env.PUBLIC_URL + '/uillinkedin.svg'} alt="Facebook Icon" style={{width: '6vw', height: 'auto', paddingRight:'1vw'}} />
+          </a>
+        </div>
+        </div>
+        <hr style={{width: '85%', borderColor: '#733CA8'}} /> {/* Horizontal line */}
+        <footer style={{textAlign: 'center',fontSize: '2vw',fontWeight: 'bold',color: 'white'}}>
+          <p> Copyright © 2022 All rights reserved | Robotics Club, IIT Kanpur</p> {/* Copyright text */}
+        </footer>
+        <br/>
+        </div>
   </React.StrictMode>
 );
 
