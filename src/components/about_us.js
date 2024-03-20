b013bb69546ad09c58aebc88a7423bb06dcde8dd
ManiPranshu <136685321+ManiPranshu@users.noreply.github.com>
2024-03-21 00:43:10 +0530

    explore page added

--- src/components/about_us.js
+++ src/components/about_us.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function AboutUs() {
+  const handleClick = () => {
+    // Handle navigation to another page
+    window.location.href = '/projects'; // Change the URL to the desired page
+  };
   return (
     <div>
       <div className="About Us" style={{ 
@@ -25,8 +29,8 @@ function AboutUs() {
           <p style={{color:'white', fontSize: '2vw', textAlign: 'left' , paddingBottom: '1vw'}}>
           If you have ever even secretly aspired to build your own robot, then the Robotics Club, IITK is the place for you! We are a diverse group of over-enthused robotics nerds who find roots across all academic departments of the institute as well as ‘seniority’. The club is a student body which finds its place under the Science and Technology Council, Student Gymkhana, IIT Kanpur. Interested in what kind of work we do? We build robots for academic purposes, to compete at both national and international events or just out of plain interest. Check out our projects and achievements. <br/><br/>#HappyRoboting
           </p>
-          <button style={{ backgroundColor: '#733CA8', height: '5vw', width: '20vw', fontSize: '1.5vw', borderRadius: '2vw' ,border: 'none'}}>
-            <p style={{color: 'white', fontSize: '1.7vw', fontWeight: 'bolder'}}>PROJECTS</p>
+          <button style={{ backgroundColor: '#733CA8', height: '5vw', width: '20vw', fontSize: '1.5vw', borderRadius: '2vw' ,border: 'none'}} >
+            <p style={{color: 'white', fontSize: '1.7vw', fontWeight: 'bolder',cursor: "pointer"}}onClick={handleClick}>PROJECTS</p>
           </button>
         </div>
       </div>
